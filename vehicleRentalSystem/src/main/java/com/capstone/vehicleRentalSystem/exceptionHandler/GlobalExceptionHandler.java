0aec973e2a73d1efd3a6153078710326935a3307
ishikasadhwani <ishikasadhwani3@gmail.com>
2025-04-17 14:46:32 +0530

    Updated BookingSevice class

--- vehicleRentalSystem/src/main/java/com/capstone/vehicleRentalSystem/exceptionHandler/GlobalExceptionHandler.java
+++ vehicleRentalSystem/src/main/java/com/capstone/vehicleRentalSystem/exceptionHandler/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.capstone.vehicleRentalSystem.exceptionHandler;
 
-import com.capstone.vehicleRentalSystem.exceptionHandler.BookingConflictException;
-import com.capstone.vehicleRentalSystem.exceptionHandler.ResourceNotFoundException;
-import com.capstone.vehicleRentalSystem.exceptionHandler.UnauthorizedAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
